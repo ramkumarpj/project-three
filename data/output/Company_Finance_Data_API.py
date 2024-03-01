79b9bf0cbf4d3af0e69ffec2121de5b746ba3ff4
Henry Nguyen <huynguyen464@gmail.com>
2024-03-01 01:13:05 -0600

    Uploaded Company_Finance_Data_API and Jupyter Notebook files

---
+++ data/output/Company_Finance_Data_API.py
@@ -0,0 +1,63 @@
+# Import dependencies
+from pymongo import MongoClient
+from pprint import pprint
+import pandas as pd
+
+# Create an instance of MongoClient
+mongo = MongoClient(port=27017)
+
+# Assign the FinanceDB database to a variable name
+db = mongo['FinanceDB']
+
+# Assign the collection to a variable
+finance = db['finance']
+companies = db['companies']
+
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/companyName")
+def companyName():
+    types = companies.companyName.unique()
+
+    all_companyName = list(np.ravel(types))
+
+    return jsonify(all_companyName)
+
+@app.route("/api/v1.0/Baker Hughes Holdings LLC")
+def BHH():
+    #Query all Baker Hughes Holding LLC data
+    query = {"companyName": 'Baker Hughes Holdings LLC'}
+
+    results = companies.find(query)
+
+    return jsonify(results)
+
+@app.route("/api/v1.0/Alphabet Inc.")
+def BHH():
+    #Query all Alphabet Inc. data
+    query = {"companyName": 'Alphabet Inc.'}
+
+    results = companies.find(query)
+
+    return jsonify(results)
+
+@app.route("/api/v1.0/Kraft Heinz Co")
+def BHH():
+    #Query all Kraft Heinz Co data
+    query = {"companyName": 'Kraft Heinz Co'}
+
+    results = companies.find(query)
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    session.close()
+    print("session closed")
