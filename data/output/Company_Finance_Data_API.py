61cd81fe63361ab7524015542892a68194868b1a
Henry Nguyen <huynguyen464@gmail.com>
2024-03-02 17:02:34 -0600

    Updated the error in Company_Finance_Data_Api.py file

--- data/output/Company_Finance_Data_API.py
+++ data/output/Company_Finance_Data_API.py
@@ -1,7 +1,7 @@
 # Import dependencies
 from pymongo import MongoClient
-from pprint import pprint
-import pandas as pd
+from flask import Flask, jsonify
+import numpy as np
 
 # Create an instance of MongoClient
 mongo = MongoClient(port=27017)
@@ -13,51 +13,45 @@ db = mongo['FinanceDB']
 finance = db['finance']
 companies = db['companies']
 
+# Initialize Flask app
 app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
 
-@app.route("/api/v1.0/companyName")
-def companyName():
-    types = companies.companyName.unique()
+@app.route("/api/v1.0/company_name")
+def company_name():
+    types = companies.distinct("companyName")
 
-    all_companyName = list(np.ravel(types))
+    return jsonify(types)
 
-    return jsonify(all_companyName)
-
-@app.route("/api/v1.0/Baker Hughes Holdings LLC")
-def BHH():
-    #Query all Baker Hughes Holding LLC data
+@app.route("/api/v1.0/Baker_Hughes_Holdings_LLC")
+def bhh():
+    # Query all Baker Hughes Holding LLC data
     query = {"companyName": 'Baker Hughes Holdings LLC'}
 
     results = companies.find(query)
 
-    return jsonify(results)
+    return jsonify(list(results))
 
-@app.route("/api/v1.0/Alphabet Inc.")
-def AI():
-    #Query all Alphabet Inc. data
+@app.route("/api/v1.0/Alphabet_Inc")
+def ai():
+    # Query all Alphabet Inc. data
     query = {"companyName": 'Alphabet Inc.'}
 
     results = companies.find(query)
 
-    return jsonify(results)
+    return jsonify(list(results))
 
-@app.route("/api/v1.0/Kraft Heinz Co")
-def KHC():
-    #Query all Kraft Heinz Co data
+@app.route("/api/v1.0/Kraft_Heinz_Co")
+def khc():
+    # Query all Kraft Heinz Co data
     query = {"companyName": 'Kraft Heinz Co'}
 
     results = companies.find(query)
 
-    return jsonify(results)
+    return jsonify(list(results))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    session.close()
-    print("session closed")
